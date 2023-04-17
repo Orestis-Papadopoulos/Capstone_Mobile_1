c85b4eee921d042ab1ad5669b98efd9412ca4106
Orestis Papadopoulos <o.papadopoulos@acg.edu>
2023-04-18 01:57:38 +0300

    Added splash screen.

---
+++ app/src/main/java/edu/acg/o/papadopoulos/capstone1/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package edu.acg.o.papadopoulos.capstone1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Window;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+// the device I used for testing ran Android 11
+// I did not use the SplashScreen API because it starts from Android 12
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // no need to show the ActionBar on splash screen
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
+        // show this Activity for 5 seconds and then navigate to MainActivity
+        new CountDownTimer(5000,1000){
+            @Override
+            public void onTick(long millisUntilFinished){}
+
+            @Override
+            public void onFinish(){
+                Intent mainActivityIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        }.start();
+    }
+}
